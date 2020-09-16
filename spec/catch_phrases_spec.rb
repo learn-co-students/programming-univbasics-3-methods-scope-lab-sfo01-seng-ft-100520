d9a766b47d24a24d629451e1ebe0b41fbc024503
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-16 18:49:17 +0000

    Done.

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
+  
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
@@ -9,6 +10,7 @@ end
 describe "toadstool" do
   it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
     expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
+    
   end
 end
 
